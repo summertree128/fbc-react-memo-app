400bc3cf2d331a0873f213335aafbeebf4d21a81
summertree128 <natsukinoue530000@gmail.com>
2022-12-07 23:03:36 +0900

    refactor export

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
-function Header() {
+export default function Header() {
   return (
     <header className="memo-app-header">
       <p>Super Simple Memo App</p>
     </header>
   );
 }
-
-export default Header;
