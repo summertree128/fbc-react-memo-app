30750dc5f65b42bc7083ed8c38c016033d5922cb
summertree128 <natsukinoue530000@gmail.com>
2022-12-07 23:06:16 +0900

    Extract AdButton

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,11 @@
+import AddButton from "./AddButton";
 import MemoList from "./MemoList";
 
 export default function Sidebar(props) {
   return (
     <aside className="memo-app-sidebar">
       <MemoList memos={props.memos} onEdit={props.onEdit} />
-      <button onClick={props.onAdd} className="memo-app-add-button">
-        +
-      </button>
+      <AddButton onClick={props.onAdd} />
     </aside>
   );
 }
